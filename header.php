6c48cee5de84c0fef0a4deeb6aa8692b24c6d3f1
Bruno Giovanni <brunobraganca@outlook.com>
2018-10-01 18:13:31 -0300

    Primeiro commit

---
+++ header.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<title><?php wp_title() ?></title>
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+	<link rel="stylesheet" type="text/css" href="<?php bloginfo('stylesheet_url'); ?>">
+
+	<?php wp_head(); ?>
+</head>
+<body>
+
+	<div class="row-fluid">
+		<div class="header">
+			<div class="header-title">
+				<h1>Bruno Giovanni</h1>
+				<h4>Desenvolvedor</h4>
+			</div>
+		</div>
+	</div>
