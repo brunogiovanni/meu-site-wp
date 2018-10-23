d8c4f85c4554f647bac64585e993ac75ebc545be
Bruno Giovanni <brunobraganca@outlook.com>
2018-10-23 17:11:24 -0300

    Classes css para esconder navbrand em telas grandes;

--- header.php
+++ header.php
@@ -36,7 +36,7 @@
 	<!-- menu -->
 	<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
 		<div class="container">
-			<a class="navbar-brand" href="<?php echo site_url(); ?>">BG</a>
+			<a class="navbar-brand d-xl-none d-lg-none d-md-none d-sm-none" href="<?php echo site_url(); ?>">BG</a>
 		  	<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
 		    	<span class="navbar-toggler-icon"></span>
 		  	</button>
